d00116c780693b98a5c394123b5446b1619aa396
Kishan Takoordyal <kishantakoordyal@gmail.com>
2018-11-19 11:35:22 +0400

    [serializers] Imported ValidatedModelSerializer from openwisp-utils #53
    closes #53

--- django_ipam/api/serializers.py
+++ django_ipam/api/serializers.py
@@ -1,39 +1,30 @@
 import swapper
+from openwisp_utils.api.serializers import ValidatedModelSerializer
 from rest_framework import serializers
 
 IpAddress = swapper.load_model('django_ipam', 'IpAddress')
 Subnet = swapper.load_model('django_ipam', 'Subnet')
 
 
-class IpRequestSerializer(serializers.ModelSerializer):
+class IpRequestSerializer(ValidatedModelSerializer):
     class Meta:
         model = IpAddress
         fields = ('subnet', 'description')
         read_only_fields = ('created', 'modified')
 
 
-class IpAddressSerializer(serializers.ModelSerializer):
+class IpAddressSerializer(ValidatedModelSerializer):
     class Meta:
         model = IpAddress
         fields = '__all__'
         read_only_fields = ('created', 'modified')
 
-    def validate(self, data):
-        instance = self.instance or self.Meta.model(**data)
-        instance.full_clean()
-        return data
 
-
-class SubnetSerializer(serializers.ModelSerializer):
+class SubnetSerializer(ValidatedModelSerializer):
     class Meta:
         model = Subnet
         fields = '__all__'
         read_only_fields = ('created', 'modified')
-
-    def validate(self, data):
-        instance = self.instance or self.Meta.model(**data)
-        instance.full_clean()
-        return data
 
 
 class ImportSubnetSerializer(serializers.Serializer):
